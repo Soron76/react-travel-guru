1b4742fa8aa930e2d8c3cdecd19f4f90119096cc
Soron76 <soron.nstu@gmail.com>
2023-10-04 23:33:43 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.envVITE_APIKEY,
+  authDomain: import.meta.envVITE_AUTHDOMAIN,
+  projectId: import.meta.envVITE_PROJECTID,
+  storageBucket: import.meta.envVITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.envVITE_MESSAGINGSENDERID,
+  appId: import.meta.envVITE_APPID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
